6df9843cce56c7281f41fe5aa372493b54810dcd
mmsalmanfaris <mmsalmanfaaris01@gmail.com>
2025-01-29 02:42:56 +0530

    SMS - Not Issued

--- manager/includes/deleteOutlet.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-include_once 'firebase.php';
- 
-if (isset($_GET['outlet_id'])) {
-  $outlet_id = $_GET['outlet_id'];
-  $database->getReference('outlets/' . $outlet_id)->remove();
-  header("Location: ../outlet/?status=datadelete");
-}
--- manager/includes/updateUser.inc.php
+++ manager/includes/updateUser.inc.php
@@ -9,15 +9,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         die("Error: Missing required field (id).");
     }
 
-    // Your update logic
-    $userId = $_POST['user_id'];
-    $data = [
-        'name' => $_POST['name'],
-        'email' => $_POST['email'],
-        'contact' => $_POST['contact'],
-        'nic' => $_POST['nic'],
-        'is_admin' => isset($_POST['isAdmin']) ? true : false,
-    ];
+    try {
+        // Your update logic
+        $userId = $_POST['user_id'];
+        $data = [
+            'name' => $_POST['name'],
+            'email' => $_POST['email'],
+            'contact' => $_POST['contact'],
+            'nic' => $_POST['nic'],
+            'is_admin' => isset($_POST['isAdmin']) ? true : false,
+        ];
 
         if (!empty($_POST['password'])) {
             $data['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
