545b15317edc45004bec0b3c174eec46d4fa2393
Ahamed Naaji <naajicreation779@gmail.com>
2025-01-30 23:34:23 +0530

    outlet edit fixed

--- admin/outlet/edit.inc.php
+++ admin/outlet/edit.inc.php
@@ -12,46 +12,50 @@ if (isset($_GET['outlet_id'])) {
                         aria-labelledby="myLargeModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                             <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">Edit Outlet</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
                                 <div class="modal-body text-black p-5">
-                                    <form action="../includes/updateUser.inc.php" method="post" class="row g-3 needs-validation"
+                                    <form action="../includes/updateOutlet.inc.php" method="post" class="row g-3 needs-validation"
                                         novalidate>
-                                        <input type="hidden" name="user_id" value="' . htmlspecialchars($outletId) . '">
-                                        <!-- Row for Name and Email -->
+                                        <input type="hidden" name="outlet_id" value="' . htmlspecialchars($outletId) . '">
+                                        
+                                        <!-- District Selection -->
+                                        <div class="mb-3">
+                                            <label for="district" class="form-label">District:</label>
+                                            <select class="form-select form-control-lg" name="district" id="district" required>
+                                                <option value="ampara"' . ($outlet['district'] == 'ampara' ? ' selected' : '') . '>Ampara</option>
+                                                <option value="batticaloa"' . ($outlet['district'] == 'batticaloa' ? ' selected' : '') . '>Batticaloa</option>
+                                                <!-- Add other district options as needed -->
+                                            </select>
+                                        </div>
+
+                                        <!-- Name and Town -->
                                         <div class="row mb-3">
                                             <div class="col-md-6">
                                                 <label for="name" class="form-label">Name:</label>
-                                                <input type="text" class="form-control form-control-lg" name="name" id="name" value="' . htmlspecialchars($outlet['district']) . '" required>
+                                                <input type="text" class="form-control form-control-lg" name="name" id="name" 
+                                                    value="' . htmlspecialchars($outlet['name']) . '" required>
                                             </div>
                                             <div class="col-md-6">
-                                                <label for="email" class="form-label">Email:</label>
-                                                <input type="email" class="form-control form-control-lg" name="email" id="email" value="' . htmlspecialchars($outlet['name']) . '" required>
+                                                <label for="town" class="form-label">Town:</label>
+                                                <input type="text" class="form-control form-control-lg" name="town" id="town" 
+                                                    value="' . htmlspecialchars($outlet['town']) . '" required>
                                             </div>
                                         </div>
 
-                                        <!-- Row for Password and Contact -->
-                                        <div class="row mb-3">
-                                            <div class="col-md-6">
-                                                <label for="password" class="form-label">Password:</label>
-                                                <input type="password" class="form-control form-control-lg" name="password" id="password">
-                                            </div>
-                                            <div class="col-md-6">
-                                                <label for="contact" class="form-label">Contact:</label>
-                                                <input type="text" class="form-control form-control-lg" name="contact" id="contact" value="' . htmlspecialchars($outlet['stock']) . '" required>
-                                            </div>
-                                        </div>
-
-                                        <!-- Row for NIC and Admin -->
-                                        <div class="row mb-4">
-                                            <div class="col-md-6">
-                                                <label for="nic" class="form-label">NIC:</label>
-                                                <input type="text" class="form-control form-control-lg" name="nic" id="nic" value="' . htmlspecialchars($outlet['town']) . '" required>
-                                            </div>
+                                        <!-- Stock -->
+                                        <div class="mb-3">
+                                            <label for="stock" class="form-label">Stock:</label>
+                                            <input type="number" class="form-control form-control-lg" name="stock" id="stock" 
+                                                value="' . htmlspecialchars($outlet['stock']) . '" required>
                                         </div>
 
                                         <!-- Submit Button -->
                                         <div class="row">
                                             <div class="col-12">
-                                                <button type="submit" class="btn btn-primary btn-lg w-100">Update</button>
+                                                <button type="submit" class="btn btn-primary btn-lg w-100">Update Outlet</button>
                                             </div>
                                         </div>
                                     </form>
